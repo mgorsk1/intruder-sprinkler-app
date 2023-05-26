4b385d68a9c51c07e85574a56a929c148ef51509
mgorsk1 <gorskimariusz13@gmail.com>
2023-05-26 10:08:21 +0200

    refactor

--- intruder_sprinkler/camera/base.py
+++ intruder_sprinkler/camera/base.py
@@ -1,6 +1,11 @@
-class CameraProvider:
+import abc
+
+
+class CameraProvider(abc.ABC):
+    @abc.abstractmethod
     def setup(self, *args, **kwarg):
         pass
 
+    @abc.abstractmethod
     def capture(self):
         pass
