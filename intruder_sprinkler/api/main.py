5a19268db4e513407da500ed07e1c30da388bc16
mgorsk1 <gorskimariusz13@gmail.com>
2023-05-27 12:11:47 +0200

    remove startup

--- intruder_sprinkler/api/main.py
+++ intruder_sprinkler/api/main.py
@@ -1,5 +1,3 @@
-import time
-
 from fastapi import FastAPI
 
 from intruder_sprinkler import config
@@ -7,15 +5,6 @@ from intruder_sprinkler.devices.sprinkler import SprinklerManager
 
 app = FastAPI()
 device = SprinklerManager(config.valve.gpio)
-
-
-@app.on_event('startup')
-async def startup_event():
-    for _ in range(3):
-        device.on()
-        time.sleep(1)
-        device.off()
-        time.sleep(1)
 
 
 @app.post('/on')
